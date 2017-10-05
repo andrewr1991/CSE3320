26c80dca9ec9d50c16ca7d2baa2a06531e7803ab
Andrew Ridout <Fridout106@gmail.com>
2017-10-04 23:13:31 -0500

    3rd commit

--- msh.c
+++ msh.c
@@ -1,6 +1,9 @@
 /*
-    Some code belongs to Trevor Bakker
-    professor at UT Arlington
+  name: Andrew Ridout
+  ID: 1001489714
+
+  Some code belongs to Trevor Bakker
+  professor at UT Arlington
 */
 
 #include <stdlib.h>
@@ -13,35 +16,64 @@ int main( void ) {
 
   while (1) {
     printf("msh> ");
-    char command1[100];
-    fgets(command1, 100, stdin);
+    char command[100];
+    fgets(command, 100, stdin);
 
-    command1[strlen(command1)-1] = '\0';
-    char path[100] = "/bin";
-    //strcat...
+    command[strlen(command)-1] = '\0';
+
+    if (strcmp(command, "exit") == 0 || strcmp(command, "quit") == 0) {
+      exit(0);
+    }
+// tokenize anythign after command and put in place of NULL in exec - up through 9 command
+// initialize all to null
+    /* Call exec with ./ - the local directory */
+    char path[100] = "./";
+    strcat(path, command);
+
+    pid_t child_pid = fork();
+    int status;
+
+    if ( child_pid == 0 ) {
+      execl(path, command, NULL );
+
+      /* Call exec with /usr/local/bin */
+      memset(path, 0, 100);
+      strncpy(path, "/usr/local/bin/", strlen("/usr/local/bin/"));
+      strcat(path, command);
+      execl(path, command, NULL );
+
+      /* Call exec with /usr/bin */
+      memset(path, 0, 100);
+      strncpy(path, "/usr/bin/", strlen("/usr/bin/"));
+      strcat(path, command);
+      execl(path, command, NULL );
+
+      /* Call exec with /bin */
+      memset(path, 0, 100);
+      strncpy(path, "/bin/", strlen("/bin/"));
+      strcat(path, command);
+      execl(path, command, NULL );
+      printf("%s: Command not found.\n", command);
+      exit( EXIT_SUCCESS );
+  }
+
+  wait( &status);
+    // strcat ./
+    // call exec
+    // strcat /usr/local/bin
+    // call execl
+    // strcat /usr/bin
+    // call execl
+    // strcat /bin
+    // call execl
+    // print command not found
 
     //if strcmp cd == 0 then change to second part of command - chdir command
 
-    if (strcmp(command1, "exit") == 0 || strcmp(command1, "quit") == 0) {
-      exit(1);
-    }
+
+
+    //else if ()
 //put everything else above the fork
-    else if (strcmp(command1, "ls") == 0) {
-      pid_t child_pid = fork();
-      int status;
-
-      if( child_pid == 0 ) {
-        execl("/bin/ls", "ls", NULL );
-        exit( EXIT_SUCCESS );
-      }
-      waitpid( child_pid, &status, 0 );
     }
-
-    else {
-      if ((int)*command1 != 0) {
-        printf("%s : Command not found.\n", command1);
-      }
-    }
-  }
   return 0;
 }
