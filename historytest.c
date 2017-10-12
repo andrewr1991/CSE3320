01a44b029adefa0b1b09f3181935c9e5385dbbf5
Andrew Ridout <Fridout106@gmail.com>
2017-10-12 18:06:10 -0500

    working on history command

---
+++ historytest.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char history[10][100];
+  strcpy(history[0], "history command 1");
+  strcpy(history[1], "history command 2");
+  strcpy(history[2], "history command 3");
+  strcpy(history[3], "history command 4");
+  strcpy(history[4], "history command 5");
+  strcpy(history[5], "history command 6");
+  strcpy(history[6], "history command 7");
+  strcpy(history[7], "history command 8");
+  strcpy(history[8], "history command 9");
+  strcpy(history[9], "history command 10");
+
+
+  for (int i = 0; i < 10; i++) {
+    printf("%s\n", history[i]);
+  }
+
+  printf("g\n");
+
+  for (int i = 0; i < 10; i++) {
+    char temp[100];
+    strcpy(temp, history[i + 1]);
+    strcpy(history[i + 1], temp);
+  }
+
+  for (int i = 0; i < 10; i++) {
+    printf("%s\n", history[i]);
+  }
+
+/*
+  for (int i = 0; i < sizeof(history)/4; i++) {
+    printf("Testing %d\n", i);
+  }
+*/
+  return 0;
+}
--- msh.c
+++ msh.c
@@ -6,74 +6,179 @@
   professor at UT Arlington
 */
 
-#include <stdlib.h>
+// The MIT License (MIT)
+//
+// Copyright (c) 2016, 2017 Trevor Bakker
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#define _GNU_SOURCE
+
+#include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
 #include <string.h>
+#include <signal.h>
+#include <stdio.h>
+
+#define WHITESPACE " \t\n"      // We want to split our command line up into tokens
+                                // so we need to define what delimits our tokens.
+                                // In this case  white space
+                                // will separate the tokens on our command line
+
+#define MAX_COMMAND_SIZE 255    // The maximum command-line size
+
+#define MAX_NUM_ARGUMENTS 10     // Mav shell only supports five arguments
 
 int main( void ) {
 
-  while (1) {
-    printf("msh> ");
-    char command[100];
-    fgets(command, 100, stdin);
+/* Parse input */
+char *token[MAX_NUM_ARGUMENTS];
 
-    command[strlen(command)-1] = '\0';
+// Declare history 2D char array
+char history[15][100];
 
-    if (strcmp(command, "exit") == 0 || strcmp(command, "quit") == 0) {
+for (int i = 0; i < 15; i++) {
+  memset(history[i], 0, 100);
+}
+
+printf("%c", history[1]);
+
+
+while (1) {
+  printf("msh> ");
+
+  int i = 0;
+  for( i = 0; i < 10; i ++ )
+  {
+    token[i] = NULL;
+  }
+
+  char * cmd_str = (char*) malloc( MAX_COMMAND_SIZE );
+
+    // Read the command from the commandline.  The
+    // maximum command that will be read is MAX_COMMAND_SIZE
+    // This while command will wait here until the user
+    // inputs something since fgets returns NULL when there
+    // is no input
+    while( !fgets (cmd_str, MAX_COMMAND_SIZE, stdin) );
+
+
+    int token_count = 0;
+
+    // Pointer to point to the token
+    // parsed by strsep
+    char *arg_ptr;
+
+    char *working_str  = strdup( cmd_str );
+
+    // we are going to move the working_str pointer so
+    // keep track of its original value so we can deallocate
+    // the correct amount at the end
+    char *working_root = working_str;
+
+    // Tokenize the input stringswith whitespace used as the delimiter
+    while ( ( (arg_ptr = strsep(&working_str, WHITESPACE ) ) != NULL) &&
+              (token_count<MAX_NUM_ARGUMENTS))
+    {
+      token[token_count] = strndup( arg_ptr, MAX_COMMAND_SIZE );
+      if( strlen( token[token_count] ) == 0 )
+      {
+        token[token_count] = NULL;
+      }
+        token_count++;
+    }
+
+    free( working_root );
+
+
+
+    // Rotate array algorithm
+    for (int i = 0; i < 15; i++) {
+      char temp[100];
+      strcpy(temp, history[i + 1]);
+      strcpy(history[i + 1], temp);
+    }
+
+    strcpy(history[0], token[0]);
+
+    for (int i = 0; i < 15; i++) {
+      printf("%s %d\n", history[i], i);
+    }
+
+
+
+
+    if( token[0] == '\0')
+      continue;
+
+    if (strcmp(token[0], "exit") == 0 || strcmp(token[0], "quit") == 0) {
       exit(0);
     }
-// tokenize anythign after command and put in place of NULL in exec - up through 9 command
+
+  if (strcmp(token[0], "cd") == 0) {
+    chdir(token[1]);
+    continue;
+  }
+// tokenize anything after command and put in place of NULL in exec - up through 9 command
 // initialize all to null
+
     /* Call exec with ./ - the local directory */
     char path[100] = "./";
-    strcat(path, command);
+    strcat(path, token[0]);
 
     pid_t child_pid = fork();
-    int status;
+
 
     if ( child_pid == 0 ) {
-      execl(path, command, NULL );
+      execl(path, token[0], token[1], token[2], token[3], token[4], token[5], token[6], token[7], token[8], token[9],NULL );
 
       /* Call exec with /usr/local/bin */
       memset(path, 0, 100);
       strncpy(path, "/usr/local/bin/", strlen("/usr/local/bin/"));
-      strcat(path, command);
-      execl(path, command, NULL );
+      strcat(path, token[0]);
+      execl(path, token[0], token[1], token[2], token[3], token[4], token[5], token[6], token[7], token[8], token[9], NULL );
 
       /* Call exec with /usr/bin */
       memset(path, 0, 100);
       strncpy(path, "/usr/bin/", strlen("/usr/bin/"));
-      strcat(path, command);
-      execl(path, command, NULL );
+      strcat(path, token[0]);
+      execl(path, token[0], token[1], token[2], token[3], token[4], token[5], token[6], token[7], token[8], token[9], NULL );
 
       /* Call exec with /bin */
       memset(path, 0, 100);
       strncpy(path, "/bin/", strlen("/bin/"));
-      strcat(path, command);
-      execl(path, command, NULL );
-      printf("%s: Command not found.\n", command);
+      strcat(path, token[0]);
+      execl(path, token[0], token[1], token[2], token[3], token[4], token[5], token[6], token[7], token[8], token[9], NULL );
+      printf("%s: Command not found.\n", token[0]);
       exit( EXIT_SUCCESS );
+
+
   }
+  else {
 
-  wait( &status);
-    // strcat ./
-    // call exec
-    // strcat /usr/local/bin
-    // call execl
-    // strcat /usr/bin
-    // call execl
-    // strcat /bin
-    // call execl
-    // print command not found
-
+    int status;
+    wait(&status);
+  }
+}
     //if strcmp cd == 0 then change to second part of command - chdir command
-
-
-
-    //else if ()
-//put everything else above the fork
-    }
   return 0;
 }
